dcc8bfc0179100fe33fec7ab9b60b20b300e90fa
RutujaKapuskar <kapuskarrutuja@gmail.com>
2023-04-21 11:12:10 +0530

    first commit

---
+++ Basic/datatype/datatype.java
@@ -0,0 +1,11 @@
+class Datatype{
+	public static void main(String[] args){
+		int x =10;
+		int y =30;
+		int z =6;
+		System.out.println(x+y);
+		System.out.println(x-y);
+		System.out.println(x/y);
+		System.out.println(x%y);
+	}
+}
